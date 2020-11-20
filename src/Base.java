21980dc4b178718dc512a057c8a822d074862e3d
o1egsey <oleg.anischenko00@gmail.com>
2020-11-20 22:44:48 +0200

    Initial commit

---
+++ src/Base.java
@@ -0,0 +1,10 @@
+public class Base {
+    int Output = 10;
+    boolean b1 = false;
+if ((b1 == true) && ((Output += 10) == 20)) {
+        System.out.println ("We are equal" + Output);
+    }
+else {
+        System.out.println ("Not equal!" + Output);
+    }
+}
